c900c1aef2b6e0b1db87d88050feefdc55400104
alexander.luchs <alexander.luchs@asurion.com>
2018-12-19 16:30:11 -0600

    Fix lint command

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -3,4 +3,10 @@
 pub get
 pub run build_runner test -- -p chrome
 
-dartanalyzer --lints --package-warnings --warnings web/main.dart
+dartanalyzer --lints --package-warnings web/main.dart | tee lint.log
+if grep '^  warning ' lint.log; then
+  echo "========================================================="
+  echo "            WARNINGS DETECTED (Failing build)"
+  echo "========================================================="
+  grep '^  warning ' lint.log
+fi
